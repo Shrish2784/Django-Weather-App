b620a0667f4ce50e69973fa8fe53cba34978f81e
shrish2784 <shrishasthana@outlook.com>
2018-06-01 22:42:39 +0530

    Fixed Bugs

--- Weather/forms.py
+++ Weather/forms.py
@@ -1,8 +1,16 @@
 from django.forms import ModelForm
+from django import forms
 from Weather.models import City
 
 
 class AddCityForm(ModelForm):
+
+    name = forms.CharField(max_length=250)
+
+    def clean(self):
+        cleaned_data = super(AddCityForm, self).clean()
+        cleaned_data['name'] = cleaned_data['name'].lower()
+
     class Meta:
         model = City
         fields = '__all__'
--- Weather/views.py
+++ Weather/views.py
@@ -12,28 +12,30 @@ class IndexView(FormView):
 
     def form_valid(self, form):
         name = form.cleaned_data['name']
-        obj = City.objects.create(name=name.lower())
+        obj = City(name=name)
+        obj.save()
         return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context_dict = super().get_context_data(**kwargs)
         url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=4c82fae2ad4ecd362a50055af2df5a54&units=metric'
         cities = City.objects.all()
+        print(len(cities))
         weather = []
-        for city in cities:
-            res = requests.get(url.format(city.name)).json()
-            name_list = city.name.split(' ')
-            name = ''
-            for word in name_list:
-                print(word)
-                name += word.capitalize()
-                name += ' '
-            city_weather = {
-                'name': name,
-                'temp': res['main']['temp'],
-                'desc': res['weather'][0]['description']
-            }
-            weather.append(city_weather)
-        print(weather)
+        if len(cities) > 0:
+            for city in cities:
+                res = requests.get(url.format(city.name)).json()
+                if res['cod'] == 200:
+                    name_list = city.name.split(' ')
+                    name = ''
+                    for word in name_list:
+                        name += word.capitalize()
+                        name += ' '
+                    city_weather = {
+                        'name': name,
+                        'temp': res['main']['temp'],
+                        'desc': res['weather'][0]['description']
+                    }
+                    weather.append(city_weather)
         context_dict['weather'] = weather
         return context_dict
